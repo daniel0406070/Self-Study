31f53e7b3d567bd824c037f845352c2e10ee5791
daniel040607 <daniel0406070@gmail.com>
2023-11-01 22:59:48 +0900

    fuuuuuuuk

--- C/memo.c
+++ C/memo.c
@@ -1,10 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 #include <string.h>
 
-int main(){
-    for(int i=300; i<3000; i++){
-        printf("%d %d\n", i, (int)floor(175*(1-pow(0.995, i))+0.5));
+int cmp(const void *a, const void *b){
+    int a_len=strlen((char *)a);
+    int b_len=strlen((char *)b);
+    
+    while(a_len>0&&b_len>0){
+        if(*(char *)a>*(char *)b){
+            return 1;
+        }
+        else if(*(char *)a<*(char *)b){
+            return -1;
+        }
+        a++;
+        b++;
+        a_len--;
+        b_len--;
     }
+
+     return a_len-b_len;
+}
+
+int main(){
+    char str[5][100];
+    for(int i=0; i<5; i++){
+        fgets(str[i], sizeof(str[i]), stdin);
+    }
+
+    qsort(str, 5, sizeof(str[0]), cmp);
+
+    for(int i=0; i<5; i++){
+        printf("%s", str[i]);
+    }
+    
 }
