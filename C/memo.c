d3185250373914a03fb919e48411f99aee9fce20
daniel040607 <daniel0406070@gmail.com>
2023-12-12 01:13:59 +0900

    10768,24723

--- C/memo.c
+++ C/memo.c
@@ -1,18 +1,23 @@
-#include <stdio.h>
-#include <math.h>
-
-
 int main(){
-    int n=32768;
-
-    printf("%d\n",n);
-    for(int i=0;i<n;i++){
-        printf("100000 ");
-    }
-    printf("\n");
-
-    printf("%d\n",n);
-    for(int i=1;i<=n;i++){
-        printf("%d ",i);
+    if(1){
+        if(1){
+            if (1)
+            {
+                if (1)
+                {
+                    if (1)
+                    {
+                        if (1)
+                        {
+                            exit(0);
+                        }
+                        
+                    }
+                    
+                }
+                
+            }
+            
+        }
     }
 }
