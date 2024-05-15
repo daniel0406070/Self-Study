e708d8dc60b5d7a018e7fead77576d31ddcca8ef
daniel040607 <daniel0406070@gmail.com>
2024-05-15 18:54:14 +0900

    6778

---
+++ C/solved/6778.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    int n,m;
+    scanf("%d %d",&n,&m);
+
+    if(3<=n&&m<=4){
+        printf("TroyMartian\n");
+    }
+    if(n<=6&&2<=m){
+        printf("VladSaturnian\n");
+    }
+    if(n<=2&&m<=3){
+        printf("GraemeMercurian\n");
+    }
+}
