2be669e50ccb02efbfa5c46b5dc93e0fc996aa63
daniel040607 <daniel0406070@gmail.com>
2024-08-19 21:49:36 +0900

    18408

---
+++ C/solved/18408.C
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    int a,b,c;
+    scanf("%d %d %d", &a, &b, &c);
+
+    if(a == b) printf("%d", a);
+    else if(a == c) printf("%d", a);
+    else printf("%d", b);
+}
